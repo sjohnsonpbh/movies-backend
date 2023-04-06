0ffc7b92b856dffe45fafe1e45a5d482053542ba
Scott Johnson <47424009+sjohnsonpbh@users.noreply.github.com>
2023-04-06 12:37:51 -0500

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,32 +1,47 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'net/http';
 
-
-
-# def news_dataset
-#   api_data = { key: best_news_secret_key }
-#   news = RestClient.get("https://newsapi.org/v2/top-headlines?   q=coronavirus&apiKey=#{api_data[:key]}")
-#   news_array = JSON.parse(news)["articles"]
-#   news_array.each do |s|
-#     Story.create(title: s["title"], author: s["author"], content:   s["content"], url: s["url"])
-#   end
-# end
-
-require 'HttpClient';
-
+puts "Seeding movies"
 def seed_movies
-  movies = HttpClient.get(`https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US&page=1`)
-  byebug
-  movie_array = JSON.parse(news)["articles"]
-  movie_array.each do |s|
-    Movie.create(title: s["title"], author: s["author"], content:   s["content"], url: s["url"])
+
+  page_num = 1
+  
+  while page_num < 50
+
+    uri = URI('http://api.themoviedb.org/3/movie/popular')
+    params = { :api_key => Rails.application.credentials.moviedb.API_KEY, :page => page_num, :language => 'en' }
+    uri.query = URI.encode_www_form(params)
+
+    res = Net::HTTP.get_response(uri)
+    # puts movies.body if movies.is_a?(Net::HTTPSuccess)
+    
+    payload = res.body 
+    
+    # Parse into ruby hash
+    payload = JSON.parse(payload)
+
+    # movies_array is misleading a bit -> should be called payload
+    # payload["results"] I believe is the movies 
+    payload["results"].each do |movie|
+
+      # Example of movie
+      # {"adult"=>false, "backdrop_path"=>"/coWUgAjqwyu2W7YFlb71rlukPKZ.jpg", "genre_ids"=>[53], "id"=>1076605, "original_language"=>"es", "original_title"=>"Cazadora", "overview"=>"Psychological thriller where the character's strength, submerged in the landscape's majesty, leads us through a tense, bleak and chilling path.", "popularity"=>983.945, "poster_path"=>"/kOD2Xg7ihr51p6eXqzaY8hX5Uq3.jpg", "release_date"=>"2023-01-19", "title"=>"The Hunteress", "video"=>false, "vote_average"=>5.9, "vote_count"=>9}
+
+      Movie.create(
+        title: movie["title"],
+        description: movie["overview"], 
+        image_path: movie["backdrop_path"])
+
+      # puts "Movie Detail"
+      # # Print movie title 
+      # puts "Title: #{movie["title"]}"
+    end
+    page_num += 1
   end
 end
+
+# puts "complete"
+
+seed_movies();
 
 
 
